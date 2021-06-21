efe3f20f3bef0f902cb8295ba4cc0fe1fb70e32f
Andrii-Rohov <andrik.la.resistance@gmail.com>
2021-06-21 19:04:22 +0300

    add closer to js code

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 let shareButton = document.getElementById("button");
 let popUp = document.getElementById("pop-up");
+let closer = document.querySelector("#closer-div");
+closer.addEventListener("click", function(){
+    popUp.classList.toggle("active");
+    popUp.classList.toggle("opacity");
+    shareButton.classList.toggle('grey-button');
+    shareButton.classList.toggle("dark-button");
+    closer.classList.toggle("active");
+
+});
 
 shareButton.onclick = function() {
     popUp.classList.toggle("active");
     popUp.classList.toggle("opacity");
-    shareButton.classList.toggle('grey-button')
+    closer.classList.toggle("active");
+    shareButton.classList.toggle('grey-button');
     shareButton.classList.toggle("dark-button");
 };
